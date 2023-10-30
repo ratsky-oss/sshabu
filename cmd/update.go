16e76b72069c2b02942ff9cee5912b5617651c4a
n.shvora <n.shvora@raceit.ru>
2023-10-30 22:01:19 +0300

    Done

--- cmd/update.go
+++ cmd/update.go
@@ -7,6 +7,8 @@ import (
 	"fmt"
 	"sshabu/pkg/compare"
 	"github.com/spf13/cobra"
+	"os"
+    "strings"
 )
 
 
@@ -20,22 +22,53 @@ and usage of using your command. For example:
 Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("-----------------------------------")
+Run: func(cmd *cobra.Command, args []string) {
+    fmt.Println("-----------------------------------")
 	
-		var (
-			firstFile compare.Bites
-			secondFile compare.Bites
-		)
-	
-		firstFile.TakeBites("./test.txt")
-		secondFile.TakeBites("./test2.txt")
-		compare.PrintCompareStrings(firstFile, secondFile)	
+    var (
+        firstFile compare.Bites
+        secondFile compare.Bites
+    )
+
+    firstFile.TakeBites("./test.txt")
+    secondFile.TakeBites("./test2.txt")
+    fmt.Println(secondFile)
+	compare.PrintCompareStrings(firstFile, secondFile)	
 		
-		fmt.Println("-----------------------------------")
-	},
+    fmt.Println("\nDo you want to overwrite? (yes/no): ")
+    if askForConfirmation() {
+        err := os.WriteFile("./test.txt", []byte(strings.Join(secondFile.Content, "\n")), 0644)
+        if err != nil {
+            fmt.Println("Error overwriting the file:", err)
+            return
+        }
+        fmt.Println("First file overwritten successfully!")
+    } else {
+        fmt.Println("Action canceled.")
+    }
+
+    fmt.Println("-----------------------------------")
+},
 }
 
+func askForConfirmation() bool {
+    var response string
+    _, err := fmt.Scanln(&response)
+    if err != nil {
+        fmt.Println("Please enter 'yes' or 'no'.")
+        return false
+    }
+    response = strings.ToLower(response)
+	switch response {
+	case "yes", "y":
+		return true
+	case "no", "n":
+		return false
+	default:
+		fmt.Println("Please enter 'yes' or 'no'.")
+        return false
+	}
+}
 
 
 func init() {
